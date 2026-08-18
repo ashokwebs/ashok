71e4c7ff1ba166cac23fe252174d2bf0a679626b
Ashok <ashokashishms@email.com>
2026-08-19 02:02:31 +0530

    fix: use type import for KeyboardEvent in system-console.tsx

--- components/system-console.tsx
+++ components/system-console.tsx
@@ -1,6 +1,6 @@
 'use client'
 
-import React, { useState, useRef, useEffect, KeyboardEvent } from 'react'
+import React, { useState, useRef, useEffect, type KeyboardEvent } from 'react'
 import { CornerDownLeft, Copy, Check } from 'lucide-react'
 
 interface HistoryEntry {
