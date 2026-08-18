46db02d0b0b108092871297173d6b4bb907b6105
Ashok <ashokashishms@email.com>
2026-08-19 01:58:35 +0530

    refactor: add React type import in system-console.tsx

--- components/system-console.tsx
+++ components/system-console.tsx
@@ -1,6 +1,6 @@
 'use client'
 
-import { useState, useRef, useEffect, KeyboardEvent } from 'react'
+import React, { useState, useRef, useEffect, KeyboardEvent } from 'react'
 import { CornerDownLeft, Copy, Check } from 'lucide-react'
 
 interface HistoryEntry {
